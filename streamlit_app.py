2e140739cad3f40092260f0ba47dac3a459c80ed
UnmeshCar <111502556+UnmeshCar@users.noreply.github.com>
2022-08-19 20:00:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My first streamlit app')
+
+streamlit.title('Test Menu')
+
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+
+streamlit.title('Spinach and Corn Sandwich')
+
+streamlit.title('Hard boiled egg')
